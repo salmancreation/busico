89c6ae29b5053967a369c441b6d7875e6d635aaf
Mehedi Hasan <sajibmehedi7@gmail.com>
2022-08-12 00:49:00 +0600

    few sections added

--- src/pages/HomeOne.js
+++ src/pages/HomeOne.js
@@ -1,14 +1,26 @@
 import BlogOne from "../Components/Blog/BlogOne";
+import BrandCarouselOne from "../Components/Brands/BrandCarouselOne";
+import CtaOne from "../Components/Cta/CtaOne";
 import FooterOne from "../Components/Footer/FooterOne";
 import HeaderOne from "../Components/Header/HeaderOne";
 import HeroOne from "../Components/Hero/HeroOne";
 import HeroSocials from "../Components/Hero/HeroSocials";
+import ServicesOne from "../Components/Services/ServicesOne";
+import TeamOne from "../Components/Team/TeamOne";
+import TestimonialOne from "../Components/Testimonial/TestimonialOne";
+import WorkProcess from "../Components/Work/WorkProcess";
 const HomeOne = () => {
     return (
         <>
         <HeaderOne/>
         <HeroSocials/>
         <HeroOne/>
+        <WorkProcess/>
+        <ServicesOne/>
+        <CtaOne/>
+        <BrandCarouselOne/>
+        <TestimonialOne/>
+        <TeamOne/>
         <BlogOne/>
         <FooterOne/>
         </>
